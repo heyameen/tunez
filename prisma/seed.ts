fc1795a5950ef7302f9b1d347f14484b1e0bbcb9
Ameen <aladeameen@gmail.com>
2023-02-02 19:15:57 +0000

    worked on data modelling

---
+++ prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { artistsData } from "./songsData";
+
+const prisma = new PrismaClient();
+
+const run = async () => {
+  await Promise.all(
+    artistsData.map(async (artist) => {
+      return prisma.artist.upsert({
+        where: { name: artist.name },
+        update: {},
+        create: {
+          name: artist.name,
+          songs: {
+            create: artist.songs.map((song) => ({
+              name: song.name,
+              duration: song.duration,
+              url: song.url,
+            })),
+          },
+        },
+      });
+    })
+  );
+
+  const salt = bcrypt.genSaltSync();
+  const user = await prisma.user.upsert({
+    where: { email: "testaccount.com" },
+    update: {},
+    create: {
+      email: "testaccount.com",
+      password: bcrypt.hashSync("password", salt),
+    },
+  });
+
+  const songs = await prisma.song.findMany({});
+  await Promise.all(
+    new Array(10).fill(1).map(async (_, i) => {
+      return prisma.playlist.create({
+        data: {
+          name: `Playlist #${i + 1}`,
+          songs: {
+            connect: songs.map((song) => ({
+              id: song.id,
+            })),
+          },
+          userId: user.id,
+        },
+      });
+    })
+  );
+};
+
+run()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
