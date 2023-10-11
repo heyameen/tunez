c9a9cda67b62cc8e5f8fe05d13f9e78dd15df495
Ameen <aladeameen@gmail.com>
2023-10-11 13:24:43 +0100

    add authentication layer to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import UserProvider from './../providers/UserProvider';
 import ModalProvider from './../providers/ModalProvider';
 import Sidebar from "@/components/Sidebar";
 import "./globals.css";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode; })
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider/>
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider/>
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,9 +5,15 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
-import Button from "./Button";
+import { FaUserAlt } from "react-icons/fa";
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { toast } from "react-hot-toast";
+
 
 import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+
+import Button from "./Button";
 
 
 interface HeaderProps {
@@ -19,10 +25,23 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
     const router = useRouter();
     const authModal = useAuthModal();
 
+    const supabaseClient = useSupabaseClient();
+    const { user } = useUser();
 
-    const handleLogout = () => {
-        //handle logout
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        // player.reset();
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+            // console.log(error)
+        } else {
+            toast.success('Logged out')
+        }
     };
+
     return (
         <div className={twMerge(`p-6`, className)}>
             <div className="w-full mb-4 flex item-center justify-between">
@@ -49,14 +68,31 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
                     </button>
                 </div>
                 <div className="flex justify-between items-center gap-x-4">
-                    <>
-                        <div>
-                            <Button onClick={authModal.onOpen} className="bg-transparent text-grey font-medium">Sign Up!</Button>
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
+                            <Button
+                                onClick={handleLogout}
+                                className="bg-orange px-6 py-2"
+                            >
+                                Logout
+                            </Button>
+                            <Button
+                                onClick={() => router.push('/account')}
+                                className="bg-orange border-r-4"
+                            >
+                                <FaUserAlt />
+                            </Button>
                         </div>
-                        <div>
-                            <Button onClick={authModal.onOpen} className="bg-orange px-4 py-2">Log in!</Button>
-                        </div>
-                    </>
+                    ) : (
+                        <>
+                            <div>
+                                <Button onClick={authModal.onOpen} className="bg-transparent text-grey font-medium">Sign Up!</Button>
+                            </div>
+                            <div>
+                                <Button onClick={authModal.onOpen} className="bg-orange px-4 py-2">Log in!</Button>
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
             {children}
